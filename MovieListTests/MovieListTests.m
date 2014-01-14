4e61846f6beb807f436f0d26fe336f6edc728367
iasandcb <iasandcb@gmail.com>
2014-01-14 15:20:50 +0900

    Initial Commit

---
+++ MovieListTests/MovieListTests.m
@@ -0,0 +1,34 @@
+//
+//  MovieListTests.m
+//  MovieListTests
+//
+//  Created by T on 2014. 1. 14..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MovieListTests : XCTestCase
+
+@end
+
+@implementation MovieListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
